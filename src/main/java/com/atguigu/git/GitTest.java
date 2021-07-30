0aebcfeb42e1f5b143bee83b0a32e7ead7609dd4
zhenghanwen <2023622784@qq.com>
2021-07-30 11:21:36 +0800

    hot-fix commit2

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -6,5 +6,6 @@ public class GitTest {
         System.out.printf("hello git2 !");
         System.out.printf("hello git3 !");
         System.out.println("hello git4 !");
+        System.out.println("hot fix test!");
     }
 }
