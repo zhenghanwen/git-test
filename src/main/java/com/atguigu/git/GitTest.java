ddef5a3be205d9dce3eeb61c534cffea5cf97192
zhenghanwen <2023622784@qq.com>
2021-07-30 11:02:01 +0800

    second commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -3,5 +3,6 @@ package com.atguigu.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.printf("hello git !");
+        System.out.printf("hello git2 !");
     }
 }
